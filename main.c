56d298520045e25027512daca95afbef6f86a21c
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-11-13 17:35:55 +0900

    실습 05

--- main.c
+++ main.c
@@ -4,28 +4,21 @@
 
 int main(void)
 {
-  int i;
-  int sum=0;
-  int grade[5];
-  int *p = grade;
+  char *pc;
+  int *pi;
+  double *pd;
+  
+  pc=(char*)10000;
+  pi=(int*)10000;
+  pd=(double*)10000;
+  
+  printf("증가 전: pc = %d, pi = %d, pd=%d\n",pc,pi,pd);
+  
+  pc++;
+  pi++;
+  pd++;
+  printf("증가 후: pc = %d, pi = %d, pd=%d\n",pc,pi,pd);
 
-  
-  for(i=0;i<5;i++)
-  {
-   printf("grade[%i]=", i);
-   scanf("%d", &grade[i]);
-  }
-  
-  
-  for(i=0;i<5;i++)
-  {
-   
-   printf("grade[%i]= %i\n", i, p[i]);
-   sum = sum + p[i];
-   
-  }
-  
-  printf("avg: %i\n", sum/5);
 
 
   system("PAUSE");	
